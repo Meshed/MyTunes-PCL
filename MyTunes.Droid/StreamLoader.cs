da9af3850c84c121f1b77a075c24b02a5886e9ed
Mark Brown <marksbrown@gmail.com>
2015-03-25 12:01:43 -0400

    Intro to Portable Class Libraries

---
+++ MyTunes.Droid/StreamLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MyTunes.Shared;
+using Android.Content;
+
+namespace MyTunes
+{
+	public class StreamLoader : IStreamLoader
+	{
+		private readonly Context context;
+
+		public StreamLoader(Context context)
+		{
+			this.context = context;
+		}
+
+		public System.IO.Stream GetStreamForFilename (string fileName)
+		{
+			return context.Assets.Open (fileName);
+		}
+	}
+}
+
